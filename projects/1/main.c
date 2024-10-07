a59dfa1c4a51c182309760281ea00d004c827161
AndrewSushi <AndrewSushi8@gmail.com>
2024-10-07 15:41:08 -0700

    Finished the basic token parsing

--- projects/1/main.c
+++ projects/1/main.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include "string_parser.h"
+#include "command.h"
 
 int main(int argc, char *argv[]){
   char *line = NULL;
   size_t len = 0;
   char input[128];
-  command_line cmd;
+  command_line large_token;
+  command_line small_token;
 
   if(argc > 1 && strcmp(argv[1], "-f") == 0){
     printf("File mode\n");
@@ -16,12 +18,18 @@ int main(int argc, char *argv[]){
     while(1){
       printf(">>> ");
       fgets(input, sizeof(input), stdin);
-      input[strcspn(input, "\n")] = '\0'; // Remove the \n character at the location that it occurs
-      
+      input[strcspn(input, "\n")] = '\0'; // Replace the \n character at the location that it occurs
+
       if(strcmp(input, "exit") == 0){
         break;
       }else{
-        printf("%s\n", input);
+        large_token = str_filler(input, ";");
+        for(int i = 0; large_token.command_list[i] != NULL; i++){
+          small_token = str_filler(large_token.command_list[i], " ");
+          for(int j = 0; small_token.command_list[j] != NULL; j++){
+            printf("%s\n", small_token.command_list[j]);
+          }
+        }
       }
     }
   }
