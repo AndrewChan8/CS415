201aa5a2568d4ca2cfdb83375db2a7a54892c221
AndrewSushi <AndrewSushi8@gmail.com>
2024-10-07 22:42:45 -0700

    Started working on the lab2 and got to the while loop section

--- labs/2/command.c
+++ labs/2/command.c
@@ -1,13 +1,42 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <unistd.h>
+#include "command.h"
+
+void write_message(char *message){
+	write(1, message, strlen(message));
+}
+
 void lfcat()
 {
 /* High level functionality you need to implement: */
-
 	/* Get the current directory with getcwd() */
+	size_t size = 1024;
+	char *cwd = malloc(size);
 	
+	if(getcwd(cwd, size) == NULL){
+		write_message("getcwd() error");
+		free(cwd);
+		return;
+	}
+
 	/* Open the dir using opendir() */
-	
+	DIR *directory;
+	struct dirent *entry;
+
+	directory = opendir(cwd);
+	if(directory == NULL){
+		write_message("opendir() error");
+		free(cwd);
+		return;
+	}
 	/* use a while loop to read the dir with readdir()*/
-	
+	while((entry = readdir(directory)) != NULL){
+		if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){ // Skip the . and .. directories
+			continue;
+		}
+	}
 		/* You can debug by printing out the filenames here */
 
 		/* Option: use an if statement to skip any names that are not readable files (e.g. ".", "..", "main.c", "lab2.exe", "output.txt" */
---
+++ labs/2/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "string_parser.h"
+#include "command.h"
+
+int main(int argc, char *argv[]){
+  char *line = NULL;
+  size_t len = 0;
+  char input[128];
+  command_line large_token;
+  command_line small_token;
+
+  if(argc > 1 && strcmp(argv[1], "-f") == 0){
+    printf("File mode\n");
+  }else{
+    printf("Interactive mode. Type 'exit' to exit.\n");
+    while(1){
+      printf(">>> ");
+      fgets(input, sizeof(input), stdin);
+      input[strcspn(input, "\n")] = '\0'; // Replace the \n character at the location that it occurs
+
+      if(strcmp(input, "exit") == 0){
+        break;
+      }else{
+        large_token = str_filler(input, ";");
+        for(int i = 0; large_token.command_list[i] != NULL; i++){
+          small_token = str_filler(large_token.command_list[i], " ");
+          for(int j = 0; small_token.command_list[j] != NULL; j++){
+            if(strcmp(small_token.command_list[j], "lfcat") == 0){
+              lfcat();
+            }else{
+              printf("%s\n", "Error: Unrecognized command!");
+            }
+          }
+          free_command_line(&small_token);
+        }
+        free_command_line(&large_token);
+      }
+    }
+  }
+  return 0;
+}
